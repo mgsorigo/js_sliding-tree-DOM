27a4378746ddd7b95039d79b9b46f9fad2b367a3
mgucma <marek.gucma@gmail.com>
2024-11-18 11:29:10 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+list.querySelectorAll('li').forEach((el) => {
+  const span = document.createElement('span');
+
+  span.textContent = el.firstChild.textContent.trim();
+  el.firstChild.replaceWith(span);
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const listItem = e.target.closest('li');
+    const subList = listItem.querySelector('ul');
+
+    if (subList) {
+      subList.style.display = subList.style.display === 'none' ? '' : 'none';
+    }
+  }
+});
